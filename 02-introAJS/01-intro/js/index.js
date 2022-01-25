b136eb24a92be74717e3d5e7df53d10a502c2a51
Mauricio Lopera <sistemas@helpex.co>
2022-01-24 22:38:51 -0500

    Intro a JS

---
+++ 02-introAJS/01-intro/js/index.js
@@ -0,0 +1,15 @@
+//Elementos para mostrar la salida de datos
+//Muestra la salida en Consola
+console.log('Hola Mundo, desde un archivo externo')
+console.log(5+10)
+console.log(4==='4')
+
+//Muestra la salida en la pagina
+document.write('Hola Mundo, desde un archivo Externo')
+document.write(5+10)
+document.write(4==='4')
+
+//Muestra un popup con la salida de datos
+alert('Hola Mundo, desde un archivo Externo')
+alert(5+10)
+alert(4==='4')
