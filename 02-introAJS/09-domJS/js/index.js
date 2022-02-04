7c797e5bed4b177d29215969a400ea76e9bac753
Mauricio Lopera <sistemas@helpex.co>
2022-02-03 22:05:57 -0500

    DOC

---
+++ 02-introAJS/09-domJS/js/index.js
@@ -0,0 +1,37 @@
+let txtNombre, añoNac, edad, añoActual;
+
+function capturaNombre ()
+{
+    txtNombre = document.getElementById('nombre');
+
+    alert('Tu nombre es ' + txtNombre.value);
+}
+
+function calcularEdad()
+{
+    let fecha = new Date();
+    03/02/2022
+    añoNac = Number(document.getElementById('fecha').value.slice(0,4));
+    añoActual = fecha.getFullYear();
+
+    if(añoActual < añoNac)
+    {
+        alert('Elige un año de nacimiento valido');
+    }
+    else
+    {
+        //obtener la edad con meses incluidos
+        edad = añoActual - añoNac;
+
+        document.getElementById('edad').innerText = 'Tu edad es: ' + edad;
+    }
+}
+
+document.getElementById('calcular').addEventListener('click', calcularEdad);
+
+function cambiarColor()
+{
+    //vuelva a tener el color azul
+    document.getElementById('cuerpo').style.backgroundColor = '#b2dfdb';
+}
+
