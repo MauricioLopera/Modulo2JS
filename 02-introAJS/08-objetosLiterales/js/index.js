c33f036f6dc9142f3ce5720675fad8d54e3be60f
jesus alberto <agito11752@gmail.com>
2022-02-03 21:09:36 -0600

    Cambio dia 030222

--- 02-introAJS/08-objetosLiterales/js/index.js
+++ 02-introAJS/08-objetosLiterales/js/index.js
@@ -5,6 +5,40 @@
 //     usaGasolina: true
 // };
 
+
+let animalesTundra ={
+    nombre:  "Lobo",
+    especie: "Mamifero",
+    patas: 4,
+    peso: 35,
+    color: "gris",
+    aullar: function() {
+        return (`El ${this.nombre} bebé aulla por su madre`)
+     },
+        comer: function(){
+            console.log(`El lobo esta comiendo una foca`)
+    }
+
+}
+
+console.log(animalesTundra.aullar());
+
+
+
+const animalTundra = ["Lobo","Mamifero",4,35 , "gris"];
+
+const [var0,var1,var2,var3,var4] = animalTundra;
+
+console.log(`El ${var0} es un ${var1}, tiene ${var2} patas, su peso es ${var3} kg y el color de su pelo es
+ ${var4}`);
+
+
+// const [nombre, edad, ...rest] = empleado;
+
+// console.info(`${nombre} tiene ${edad}`); 
+// console.info(`los datos completos son: ${rest}`);
+
+
 // console.log(carro.color);
 // console.log(carro.puertas);
 
@@ -44,25 +78,25 @@
 
 // destructuracion 
 
-const animales = ["Conejo","Gato","Perro","Rana"];
-const [animal1,animal2,animal3,animal4] = animales;
+// const animales = ["Conejo","Gato","Perro","Rana"];
+// const [animal1,animal2,animal3,animal4] = animales;
 
-console.log(`Mi primer mascota fue: ${animal1} y ahora tengo ${animal2}, ${animal3}, ${animal4}`);
+// console.log(`Mi primer mascota fue: ${animal1} y ahora tengo ${animal2}, ${animal3}, ${animal4}`);
 
-// ---- destructura lo que necesites ----
+// // ---- destructura lo que necesites ----
 
-const nombres = ["Zanahoria","Mei","Taco","Rene"];
+// const nombres = ["Zanahoria","Mei","Taco","Rene"];
 
-const [nombre0, , nombre2] = nombres;
+// const [nombre0, , nombre2] = nombres;
 
-console.info(`Mis mascotas son: ${nombre0} y ${nombre2}`);
+// console.info(`Mis mascotas son: ${nombre0} y ${nombre2}`);
 
-// Uso del operador rest
+// // Uso del operador rest
 
 
-const empleado = ["Emilio", 24, "Puebla","Desarrollador front (El chido)", true];
+// const empleado = ["Emilio", 24, "Puebla","Desarrollador front (El chido)", true];
 
-const [nombre, edad, ...rest] = empleado;
+// const [nombre, edad, ...rest] = empleado;
 
-console.info(`${nombre} tiene ${edad}`); 
-console.info(`los datos completos son: ${rest}`);
+// console.info(`${nombre} tiene ${edad}`); 
+// console.info(`los datos completos son: ${rest}`);
